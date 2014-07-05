d155cba165a02acb9eb155381bd1d42abfa15d98
dump3r <dev@blaargh.de>
2014-07-05 21:41:56 +0200

    PHPUnit-Tests für die URL-Library geschrieben und die Tests angepasst

--- config/tests.php
+++ config/tests.php
@@ -2,3 +2,41 @@
     
     if(!defined('DCMS_SECURE'))
         exit('Verboten!');
+
+    /**
+     * Konfigurationsdatei für Anfragen an die index.php
+     * -------------------------------------------------
+     * Diese Datei wird nur geladen, wenn eine Anfrage an
+     * die index.php gestellt wird.
+     */
+
+    /**
+     * Projekteinstellungen
+     * --------------------
+     * Hier wird unter anderem der Projekttitel festgelegt.
+     */
+    \dcms\Config::set('app_title', 'Projekttitel');
+    
+    /**
+     * Themeeinstellungen
+     * ------------------
+     */
+    \dcms\Config::set('theme_name', '');
+    \dcms\Config::set('theme_extension', '.php');
+    
+    /**
+     * URL-Einstellungen
+     * -----------------
+     */
+    \dcms\Config::set('url_index', 'index.php');
+    \dcms\Config::set('url_pattern', 'a-zA-Z0-9./-_');
+    \dcms\Config::set('url_rewrite', false);
+    
+    /**
+     * Routereinstellungen
+     * -------------------
+     * Hier wird eine Standardroute und eine Fehlerroute für 404 Fehler
+     * definiert.
+     */
+    \dcms\Config::set('route_default', 'welcome/index');
+    \dcms\Config::set('route_404', 'welcome/page_missing');
--- tests/framework/libraries/UrlTest.php
+++ tests/framework/libraries/UrlTest.php
@@ -2,9 +2,59 @@
 
     class UrlTest extends \PHPUnit_Framework_TestCase {
         
-        public function testParseString()
+        public function testSegment()
         {
+            $_SERVER['PATH_INFO'] = '/welcome/index';
+            $url = new \dcms\library\Url();
             
+            $this->assertEquals('welcome', $url->segment(1));
+            $this->assertEquals('index', $url->segment(2));
+        }
+        
+        public function testParseStringNoString()
+        {
+            /* @var $url \dcms\library\Url */
+            $url = \dcms\library\Url::get();
+            
+            $wannabe_string = array();
+            $result = $url->parse_string($wannabe_string);
+            
+            $this->assertFalse($result);
+        }
+        
+        public function testParseStringFalse()
+        {
+            /* @var $url \dcms\library\Url */
+            $url = \dcms\library\Url::get();
+            
+            $string = 'welcome/jksdfhkü=)()';
+            $result = $url->parse_string($string);
+            
+            $this->assertFalse($result);
+        }
+        
+        public function testParseStringArray()
+        {
+            /* @var $url \dcms\library\Url */
+            $url = \dcms\library\Url::get();
+            
+            $string = 'welcome/index';
+            $result_expected = array('welcome', 'index');
+            $result_returned = $url->parse_string($string);
+            
+            $this->assertEquals($result_expected, $result_returned);
+        }
+        
+        public function testParseStringAutofill()
+        {
+            /* @var $url \dcms\library\Url */
+            $url = \dcms\library\Url::get();
+            
+            $string = 'welcome';
+            $result_expected = array('welcome', 'index');
+            $result_returned = $url->parse_string($string);
+            
+            $this->assertEquals($result_expected, $result_returned);
         }
         
     }
