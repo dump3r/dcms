2d9ceca731c56511aea464a674961cf3ca6e2352
dump3r <dev@blaargh.de>
2014-07-05 04:27:36 +0200

    Arbeiten an der statischen Errorklasse fortgesetzt

---
+++ framework/core/Error.php
@@ -0,0 +1,19 @@
+<?php
+
+    namespace dcms;
+    
+    if(!defined('DCMS_SECURE'))
+        exit('Verboten!');
+    
+    /**
+     * Die Errorklasse wird verwendet um Fehlermeldungen und
+     * Informationen über das System im HTML-Format anzuzeigen.
+     * 
+     * @author dump3r
+     * @version 1.0.0
+     * @see http://blaargh.de/dcms/docs/core/Error
+     */
+    class Error {
+
+        
+    }
